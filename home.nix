58d3f211521302764ceec26121091098c26cb21b
Steven Tsang <tsangste@gmail.com>
2024-10-25 23:18:28 +0100

    Add zsh configuration and setup oh-my-zsh

--- home.nix
+++ home.nix
@@ -70,6 +70,28 @@
     # EDITOR = "emacs";
   };
 
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      ll = "ls -l";
+      update = "sudo nixos-rebuild switch";
+    };
+    history = {
+      size = 10000;
+      path = "${config.xdg.dataHome}/zsh/history";
+    };
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" "aws" ];
+      theme = "pygmalion";
+    };
+  };
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
