7ab9a8754e1c5ea3700195372ae538a7d01b4ffc
Steven Tsang <tsangste@gmail.com>
2024-11-07 00:08:32 +0000

    Move git gpg sign config into home-manager

--- home.nix
+++ home.nix
@@ -126,6 +126,12 @@
       d = "diff";
       s = "status";
     };
+    extraConfig = {
+      commit.gpgsign = true;
+      gpg.format = "ssh";
+      gpg.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+      user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDNvWJnENwYza2ab/ALsmG4yvXxM1KvJriJuE9LpTpZj";
+    };
     includes = [
       {
         path = "~/.gitconfig.personal";
