9417d60c7dc0e40fe253ab9c8f7d6b40c61b4a58
Steven Tsang <tsangste@gmail.com>
2024-12-29 17:40:06 +0000

    Refactor darwin configuration creation into a function

--- flake.nix
+++ flake.nix
@@ -19,100 +19,84 @@
       fullname = "Steven Tsang";
       username = "steven.tsang";
       email = "tsangste@gmail.com";
+
+      mkSystem = { darwin, host, extraArgs }:
+        darwin.lib.darwinSystem {
+          modules = [
+            ./modules/system/configuration.nix
+            ({ lib, ... }: {
+              inherit self;
+              inherit (extraArgs // { brews = [ ]; casks = [ ]; masApps = { }; }) brews casks masApps;
+            })
+            nix-homebrew.darwinModules.nix-homebrew
+            {
+              nix-homebrew = import ./modules/homebrew { inherit username; };
+            }
+            {
+              users.users.${username}.home = "/Users/${username}";
+            }
+            home-manager.darwinModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users.${username} = import ./home/hosts/${host};
+                extraSpecialArgs = {
+                  inherit fullname username email;
+                };
+              };
+            }
+          ]
+          ++ (extraArgs.extraModules or [ ]); # Dynamically append extra Nix modules using extraArgs
+        };
     in
     {
       # Build darwin flake using:
       # $ darwin-rebuild build --flake .#simple
       darwinConfigurations = {
-        "mini" = nix-darwin.lib.darwinSystem {
-          modules =
-            [
-              ./modules/system/configuration.nix
-              ({ lib, ... }: {
-                inherit self;
-                masApps = {
-                  "UTM Virtual Machines" = 1538878817;
-                };
-              })
-              nix-homebrew.darwinModules.nix-homebrew
-              {
-                nix-homebrew = import ./modules/homebrew { inherit username; };
-              }
-              {
-                users.users.${username}.home = "/Users/${username}";
-              }
-              home-manager.darwinModules.home-manager
-              {
-                home-manager = {
-                  useGlobalPkgs = true;
-                  useUserPackages = true;
-                  users.${username} = import ./home/hosts/mini;
-                  extraSpecialArgs = {
-                    inherit fullname;
-                    inherit username;
-                    inherit email;
-                  };
-                };
-              }
-            ];
+        "mini" = mkSystem {
+          darwin = nix-darwin;
+          host = "mini";
+          extraArgs = {
+            masApps = {
+              "UTM Virtual Machines" = 1538878817;
+            };
+          };
         };
-        "work" = nix-darwin.lib.darwinSystem {
-          modules =
-            [
-              ./modules/system/configuration.nix
-              ({ lib, ... }: {
-                inherit self;
-                brews = [
-                  "awscli"
-                  "aws-iam-authenticator"
-                  "cairo"
-                  "checkov"
-                  "credstash"
-                  "fnm"
-                  "helm"
-                  "kubectl"
-                  "pango"
-                  "pixman"
-                  "pipx"
-                  "pre-commit"
-                  "pyenv-virtualenv"
-                  "python-setuptools"
-                  "pipenv"
-                  "terraform-docs"
-                  "tilt"
-                  "tfenv"
-                  "tflint"
-                  "yarn"
-                ];
-                casks = [
-                  "docker"
-                  "lens"
-                  "postman"
-                  "session-manager-plugin"
-                  "Tuple"
-                ];
-              })
-              nix-homebrew.darwinModules.nix-homebrew
-              {
-                nix-homebrew = import ./modules/homebrew { inherit username; };
-              }
-              {
-                users.users.${username}.home = "/Users/${username}";
-              }
-              home-manager.darwinModules.home-manager
-              {
-                home-manager = {
-                  useGlobalPkgs = true;
-                  useUserPackages = true;
-                  users.${username} = import ./home/hosts/work;
-                  extraSpecialArgs = {
-                    inherit fullname;
-                    inherit username;
-                    inherit email;
-                  };
-                };
-              }
+        "work" = mkSystem {
+          darwin = nix-darwin;
+          host = "work";
+          extraArgs = {
+            brews = [
+              "awscli"
+              "aws-iam-authenticator"
+              "cairo"
+              "checkov"
+              "credstash"
+              "fnm"
+              "helm"
+              "kubectl"
+              "pango"
+              "pixman"
+              "pipx"
+              "pre-commit"
+              "pyenv-virtualenv"
+              "python-setuptools"
+              "pipenv"
+              "terraform-docs"
+              "tilt"
+              "tfenv"
+              "tflint"
+              "yarn"
             ];
+            casks = [
+              "docker"
+              "lens"
+              "postman"
+              "session-manager-plugin"
+              "Tuple"
+            ];
+          };
         };
       };
     };
