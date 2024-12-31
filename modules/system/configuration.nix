950460ed788330a3e729647cdae965836ddbf8f1
Steven Tsang <tsangste@gmail.com>
2024-12-31 01:18:07 +0000

    Add activation script to reload mac configurations

--- modules/system/configuration.nix
+++ modules/system/configuration.nix
@@ -102,6 +102,11 @@ with lib;
         done
       '';
 
+    system.activationScripts.postUserActivation.text = ''
+      # Following line should allow us to avoid a logout/login cycle
+      /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+    '';
+
     system.defaults = {
       dock.autohide = true;
       finder.FXPreferredViewStyle = "clmv";
