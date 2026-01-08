f3e0352fd766642bceefd0e94c0866d21e3c2596
Steven Tsang <3403544+tsangste@users.noreply.github.com>
2026-01-08 23:39:35 +0000

    autostart 1Password GUI service

--- modules/nixos/home-manager.nix
+++ modules/nixos/home-manager.nix
@@ -12,4 +12,19 @@
   programs.git.settings = {
     gpg."ssh".program = "${pkgs._1password-gui}/bin/op-ssh-sign";
   };
+
+  systemd.user.services."1password" = {
+    Unit = {
+      Description = "1Password GUI";
+      After = [ "graphical-session-pre.target" ];
+      PartOf = [ "graphical-session.target" ];
+    };
+    Service = {
+      ExecStart = "${pkgs._1password}/bin/1password --silent";
+      Restart = "on-failure";
+    };
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+  };
 }
