4e50c998c0a15a424c354fe758980bcc58957b66
Steven Tsang <3403544+tsangste@users.noreply.github.com>
2026-01-08 23:47:49 +0000

    work around display issue

--- modules/nixos/home-manager.nix
+++ modules/nixos/home-manager.nix
@@ -13,18 +13,26 @@
     gpg."ssh".program = "${pkgs._1password-gui}/bin/op-ssh-sign";
   };
 
-  systemd.user.services."1password" = {
+  systemd.user.services._1password = {
     Unit = {
       Description = "1Password GUI";
-      After = [ "graphical-session-pre.target" ];
-      PartOf = [ "graphical-session.target" ];
     };
     Service = {
-      ExecStart = "${pkgs._1password}/bin/1password --silent";
+      ExecStart = "${pkgs._1password-gui}/bin/1password --silent";
       Restart = "on-failure";
+      RestartSec = "5s";
+      Environment = [
+        "PATH=${pkgs.coreutils}/bin:${pkgs.dbus}/bin"
+      ];
     };
     Install = {
-      WantedBy = [ "graphical-session.target" ];
+      WantedBy = [ "default.target" ];
     };
   };
+  programs.zsh.initExtra = ''
+    if [ -n "$DISPLAY" ]; then
+      systemctl --user import-environment DISPLAY WAYLAND_DISPLAY
+      dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY
+    fi
+  '';
 }
